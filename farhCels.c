f260830c511ae569bf45696406c9716385fccb09
RodrigoLista <rodrigo_lista@hotmail.com>
2015-04-30 17:29:01 -0300

    segundo comit

--- farhCels.c
+++ farhCels.c
@@ -8,27 +8,61 @@
  ============================================================================
  */
 
+
+/* Lista Rodrigo
+
+ * 30/04/2015
+ 
+*k2051
+* Este programa convierte temperatura que se encuentran en grados Fahrenheit (de 0 a 300)
+  a grados Celsius. Y *muestra por pantalla dos columnas, una con la temperatura 
+
+ * en grados Fahrenheit y al lado su conversion en Celsius.
+
+*/
+
+
+
+
+
+
+
 #include <stdio.h>
+
 
 float calcularCels(int);
 
+
 int main(void) {
+
 const inicio=0;
+
 const fin=300;
+
 const incremento=20;
+
 
 int farh;
 float cels;
+
    for(farh=inicio;farh<=fin;farh=farh+incremento)
-       {cels=calcularCels(farh);
+       
+{cels=calcularCels(farh);
+
          printf("%3d %6.1f\n",farh,cels);
+
        }
+
 
 return 0;
 
 }
 
+
+
 float calcularCels(int farhenheit){
-   float celsius=(5.0/9.0)*(farhenheit-32);
+   
+float celsius=(5.0/9.0)*(farhenheit-32);
+
    return celsius;
 }
